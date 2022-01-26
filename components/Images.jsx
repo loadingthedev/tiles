12742bed19b26e06ad3201753d84ea3ba2e79e33
loadingthelife <loadingthedev@gmail.com>
2022-01-27 02:02:42 +0530

    showing images in black tile for test

--- components/Images.jsx
+++ components/Images.jsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import React from "react";
 import Upload from "./Upload";
+import blackTile from "../public/img/tiles/black.05456af9.svg";
 
 function Images({ url }) {
   console.log(url);
   return (
     <>
-      <div className="d-flex" style={{ gap: "1rem" }}>
-        <Image src={url} height={200} width={200} />
+      <div className="d-flex" style={{ gap: "1rem", position: "relative" }}>
+        {/* <Image src={blackTile} width={250} height={250} className="TileFrame" /> */}
+        <img src="img/tiles/black.05456af9.svg" alt="" className="TileFrame" />
+        <img src={url} alt="" className="TileImage" />
+        {/* <Image src={url} height={200} width={200} className="TileImage" /> */}
       </div>
     </>
   );
