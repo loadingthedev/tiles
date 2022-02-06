bc23e42a8f660d2b182c61ddc37aa4a99a579d5c
loadingthelife <loadingthedev@gmail.com>
2022-02-07 00:22:36 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../lib/prisma";
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 export default NextAuth({
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
